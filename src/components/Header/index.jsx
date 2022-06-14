46871e47797d3e435261f0bd054cde42f8c64a77
Jood80 <shawar.nujood@gmail.com>
2022-06-14 21:32:29 +0300

    feat(Header): create header comonent

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import logoUrl from '../../assets/logo.svg'
+import './Header.css'
+
+const Header=() => (
+	<div className="header">
+		<img src={logoUrl} alt="Tesla" />
+	</div>
+)
+
+export default Header
