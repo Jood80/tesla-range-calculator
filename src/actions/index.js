dd08ce4d2ca54718146905d8622eedf823643ee9
Jood80 <shawar.nujood@gmail.com>
2022-06-16 21:08:25 +0300

    chore(redux): define actions

---
+++ src/actions/index.js
@@ -0,0 +1,56 @@
+import {counterDefaultValue} from '../constants/counterDefaultValue'
+
+export const speedUp=(value) => {
+	return {
+		type: 'SPEED_UP',
+		value,
+		step: counterDefaultValue.speed.step,
+		maxValue: counterDefaultValue.speed.max
+	}
+}
+
+export const speedDown=(value) => {
+	return {
+		type: 'SPEED_DOWN',
+		value,
+		step: counterDefaultValue.speed.step,
+		minValue: counterDefaultValue.speed.min
+	}
+}
+
+export const temperatureUp=(value) => {
+	return {
+		type: 'TEMERATURE_UP',
+		value,
+		step: counterDefaultValue.temperature.step,
+		maxValue: counterDefaultValue.temperature.max
+	}
+}
+
+export const temperatureDown=(value) => {
+	return {
+		type: 'TEMERATURE_DOWN',
+		value,
+		step: counterDefaultValue.temperature.step,
+		maxValue: counterDefaultValue.temperature.min
+	}
+}
+
+export const changeClimate=() => {
+	return {
+		type: 'CHANGE_CLIMATE'
+	}
+}
+
+export const changeWheel=(value) => {
+	return {
+		type: 'CHANGE_WHEEL',
+		value
+	}
+}
+
+export const updateStats=() => {
+	return {
+		type: 'UPDATE_STATS'
+	}
+}
