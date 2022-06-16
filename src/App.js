4b1ecd40ce26a9d170342868fd97128cf3307cad
Jood80 <shawar.nujood@gmail.com>
2022-06-16 13:32:06 +0300

    fix(style): update App className

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const counterDefaultVal={
 
 const App=() => {
   return (
-    <div className="wrapper">
+    <div className="App">
       <Header />
       <TeslaBattery counterDefaultVal={counterDefaultVal} />
     </div>
