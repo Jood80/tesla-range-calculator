086fbdba8c55f45a4011ff46dd6ef25b83566b56
Jood80 <shawar.nujood@gmail.com>
2022-06-17 11:30:10 +0300

    Revert "ci(redux): test those cases <actions>"
    This reverts commit f5818edfbe8150e75f0c9cf03465db1d7db599fd.

--- src/reducers/teslaRangeApp.spec.js
+++ src/reducers/teslaRangeApp.spec.js
@@ -1,5 +1,5 @@
 import appReducer from "./teslaRangeApp";
-import {climateChangeState, initialState, speedDownState, speedUpState, temperatureDownState, temperatureUpState, wheelChangeState} from "./testData";
+import {climateChangeState, initialState} from "./testData";
 
 describe('test our lovely reducer', () => {
 	it('should handle initial stat', () => {
@@ -8,65 +8,12 @@ describe('test our lovely reducer', () => {
 		).toEqual(initialState)
 	})
 
-	it('should handle SPEED_UP', () => {
-		expect(
-			appReducer(climateChangeState, {
-				type: 'SPEED_UP',
-				value: 55,
-				step: 5,
-				maxValue: 70
-			})
-		).toEqual(speedUpState)
-	})
-
-	it('should handle SPEED_DOWN', () => {
-		expect(
-			appReducer(speedUpState, {
-				type: 'SPEED_DOWN',
-				value: 60,
-				step: 5,
-				minValue: 45
-			})
-		).toEqual(speedDownState)
-	})
-
-	it('should handle CHANGE_WHEEL', () => {
-		expect(
-			appReducer(speedDownState, {
-				type: 'CHANGE_WHEEL',
-				value: 21
-			})
-		).toEqual(wheelChangeState)
-	})
-
-	it('should handle TEMPERATURE_UP', () => {
-		expect(
-			appReducer(wheelChangeState, {
-				type: 'TEMPERATURE_UP',
-				value: 20,
-				step: 10,
-				maxValue: 40
-			})
-		).toEqual(temperatureUpState)
-	})
-
 	it('should handle CHANGE_CLIMATE', () => {
 		expect(
 			appReducer(initialState, {
 				type: 'CHANGE_CLIMATE'
 			})
 		).toEqual(climateChangeState)
-	})
-
-	it('should handle TEMPERATURE_DOWN', () => {
-		expect(
-			appReducer(temperatureUpState, {
-				type: 'TEMPERATURE_DOWN',
-				value: 30,
-				step: 10,
-				minValue: -10
-			})
-		).toEqual(temperatureDownState)
 	})
 })
 
--- src/reducers/testData.js
+++ src/reducers/testData.js
@@ -15,74 +15,6 @@ export const initialState={
   }
 }
 
-export const speedUpState={
-  carstats: [
-    {miles: 242, model: "60"},
-    {miles: 248, model: "60D"},
-    {miles: 292, model: "75"},
-    {miles: 303, model: "75D"},
-    {miles: 332, model: "90D"},
-    {miles: 371, model: "P100D"}
-  ],
-  config: {
-    speed: 60,
-    temperature: 20,
-    climate: false,
-    wheels: 19
-  }
-}
-
-export const speedDownState={
-  carstats: [
-    {miles: 267, model: "60"},
-    {miles: 273, model: "60D"},
-    {miles: 323, model: "75"},
-    {miles: 334, model: "75D"},
-    {miles: 366, model: "90D"},
-    {miles: 409, model: "P100D"}
-  ],
-  config: {
-    speed: 55,
-    temperature: 20,
-    climate: false,
-    wheels: 19
-  }
-}
-
-export const temperatureUpState={
-  carstats: [
-    {miles: 264, model: "60"},
-    {miles: 272, model: "60D"},
-    {miles: 319, model: "75"},
-    {miles: 333, model: "75D"},
-    {miles: 367, model: "90D"},
-    {miles: 398, model: "P100D"}
-  ],
-  config: {
-    speed: 55,
-    temperature: 30,
-    climate: false,
-    wheels: 21
-  }
-}
-
-export const temperatureDownState={
-  carstats: [
-    {miles: 261, model: "60"},
-    {miles: 268, model: "60D"},
-    {miles: 316, model: "75"},
-    {miles: 327, model: "75D"},
-    {miles: 359, model: "90D"},
-    {miles: 389, model: "P100D"}
-  ],
-  config: {
-    speed: 55,
-    temperature: 20,
-    climate: false,
-    wheels: 21
-  }
-}
-
 export const climateChangeState={
   carstats: [
     {miles: 267, model: "60"},
@@ -97,22 +29,5 @@ export const climateChangeState={
     temperature: 20,
     climate: false,
     wheels: 19
-  }
-}
-
-export const wheelChangeState={
-  carstats: [
-    {miles: 261, model: "60"},
-    {miles: 268, model: "60D"},
-    {miles: 316, model: "75"},
-    {miles: 327, model: "75D"},
-    {miles: 359, model: "90D"},
-    {miles: 389, model: "P100D"}
-  ],
-  config: {
-    speed: 55,
-    temperature: 20,
-    climate: false,
-    wheels: 21
   }
 }
